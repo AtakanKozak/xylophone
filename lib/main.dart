fd9318d533db018b1d1aa44f1c78434ec201bf08
AtakanKozak <atakankozak2003@gmail.com>
2021-10-12 22:08:33 +0300

    ilk yükleme

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'package:xylophone/pages/splash.dart';
 
-void main() => runApp(Sizer(builder: (context, orientation, deviceType) {
+void main() => runApp(
+    Sizer(builder: (context, orientation, deviceType) {
       return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: SplashScreen(),
--- lib/pages/xylophone.dart
+++ lib/pages/xylophone.dart
@@ -1,10 +1,46 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:sizer/sizer.dart';
+import 'package:xylophone/ad_helper.dart';
 
 
-void main() => runApp(XylophoneApp());
+const AndroidNotificationChannel channel= AndroidNotificationChannel(
+    "high_importance_channel",
+    "High importance Notifications",
+    "This channel is used for important notifications",
+    importance: Importance.high,
+  playSound: true
+);
+
+Future<void>_firebaseMessagingBackgroundHandler(RemoteMessage message)async{
+  await Firebase.initializeApp();
+  print("A bg message just showed up : ${message.messageId}");
+}
+
+
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  runApp(XylophoneApp());
+}
 
 class XylophoneApp extends StatefulWidget {
   @override
@@ -12,15 +48,95 @@ class XylophoneApp extends StatefulWidget {
 }
 
 class _XylophoneAppState extends State<XylophoneApp> {
-  final player = AudioCache();
+  BannerAd _ad;
+  bool isloaded;
 
+  @override
+  void initState() {
+    super.initState();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message){
+      RemoteNotification notification =message.notification;
+      AndroidNotification android=message.notification?.android;
+      if(notification !=null && android !=null){
+
+      }
+      FirebaseMessaging.onMessage.listen((RemoteMessage message ){
+        print("A new onMessageOpenedApp event was published!");
+        RemoteNotification notification=message.notification,
+        AndroidNotification; android=message.notification?.android;
+        if(notification !=null&& android!=null){
+          showDialog(context: context, builder: (_){
+            return AlertDialog(
+             title: Text(notification.title),
+              content: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(notification.body),
+                  ],
+                ),
+              ),
+            );
+          });
+        }
+          NotificationDetails(
+          android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channel.description,
+          importance: Importance.high,
+          color: Colors.blue,
+          playSound: true,
+          icon: "@mipmap/ic_launcher"
+          )
+          );
+        }
+      );
+    });
+
+    _ad = BannerAd(
+        adUnitId: AdHelper.bannerAdUnitId,
+        request: AdRequest(),
+        size: AdSize.banner,
+        listener: BannerAdListener(onAdLoaded: (_) {
+          setState(() {
+            isloaded = true;
+          });
+        }, onAdFailedToLoad: (_, error) {
+          print("Ad Failed to load with error:$error");
+        }));
+    _ad.load();
+  }
+
+  @override
+  void dispose() {
+    _ad?.dispose();
+    super.dispose();
+  }
+
+  Widget checkForAd() {
+    if (isloaded == true) {
+      return Container(
+        child: AdWidget(
+          ad: _ad,
+        ),
+        width: _ad.size.width.toDouble(),
+        height: _ad.size.height.toDouble(),
+        alignment: Alignment.center,
+      );
+    } else {
+      return CircularProgressIndicator();
+    }
+  }
+
+  final player = AudioCache();
 
   Color color1 = Color(0xFF1976D2);
   Color color2 = Color(0xFF1E88E5);
   Color color3 = Color(0xFF2196F3);
   Color color4 = Color(0xFF42A5F5);
   Color color5 = Color(0xFF64B5F6);
-  Color color6=  Color(0xFF90CAF9);
+  Color color6 = Color(0xFF90CAF9);
   Color color7 = Color(0xFFBBDEFB);
   @override
   Widget build(BuildContext context) {
@@ -38,9 +154,13 @@ class _XylophoneAppState extends State<XylophoneApp> {
                     player.play('sound/note1.wav');
                   },
                   child: Container(
-                    child: Center(child: Text("DO",style:TextStyle(fontSize: 20,fontFamily: "Niveau"),)),
+                    child: Center(
+                        child: Text(
+                      "DO    C",
+                      style: TextStyle(fontSize: 20, fontFamily: "Niveau"),
+                    )),
                     width: double.infinity,
-                    height: 13.h,
+                    height: 10.h,
                     color: color1,
                   ),
                 ),
@@ -49,9 +169,13 @@ class _XylophoneAppState extends State<XylophoneApp> {
                     player.play('sound/note2.wav');
                   },
                   child: Container(
-                    child: Center(child: Text("RE",style:TextStyle(fontSize: 20,fontFamily: "Niveau"),)),
-                  width: double.infinity,
-                    height: 13.h,
+                    child: Center(
+                        child: Text(
+                      "RE     D",
+                      style: TextStyle(fontSize: 20, fontFamily: "Niveau"),
+                    )),
+                    width: double.infinity,
+                    height: 10.h,
                     color: color2,
                   ),
                 ),
@@ -60,9 +184,13 @@ class _XylophoneAppState extends State<XylophoneApp> {
                     player.play('sound/note3.wav');
                   },
                   child: Container(
-                    child: Center(child: Text("Mİ",style:TextStyle(fontSize: 20,fontFamily: "Niveau"),)),
+                    child: Center(
+                        child: Text(
+                      "Mİ     E",
+                      style: TextStyle(fontSize: 20, fontFamily: "Niveau"),
+                    )),
                     width: double.infinity,
-                    height: 13.h,
+                    height: 10.h,
                     color: color3,
                   ),
                 ),
@@ -71,9 +199,13 @@ class _XylophoneAppState extends State<XylophoneApp> {
                     player.play('sound/note4.wav');
                   },
                   child: Container(
-                    child: Center(child: Text("FA",style:TextStyle(fontSize: 20,fontFamily: "Niveau"),)),
+                    child: Center(
+                        child: Text(
+                      "FA     F",
+                      style: TextStyle(fontSize: 20, fontFamily: "Niveau"),
+                    )),
                     width: double.infinity,
-                    height: 13.h,
+                    height: 10.h,
                     color: color4,
                   ),
                 ),
@@ -82,9 +214,13 @@ class _XylophoneAppState extends State<XylophoneApp> {
                     player.play('sound/note5.wav');
                   },
                   child: Container(
-                    child: Center(child: Text("SOL",style:TextStyle(fontSize: 20,fontFamily: "Niveau"),)),
+                    child: Center(
+                        child: Text(
+                      "SOL   G",
+                      style: TextStyle(fontSize: 20, fontFamily: "Niveau"),
+                    )),
                     width: double.infinity,
-                    height: 13.h,
+                    height: 10.h,
                     color: color5,
                   ),
                 ),
@@ -93,9 +229,13 @@ class _XylophoneAppState extends State<XylophoneApp> {
                     player.play('sound/note6.wav');
                   },
                   child: Container(
-                    child: Center(child: Text("LA",style:TextStyle(fontSize: 20,fontFamily: "Niveau"),)),
+                    child: Center(
+                        child: Text(
+                      "LA     A",
+                      style: TextStyle(fontSize: 20, fontFamily: "Niveau"),
+                    )),
                     width: double.infinity,
-                    height: 13.h,
+                    height: 10.h,
                     color: color6,
                   ),
                 ),
@@ -104,12 +244,17 @@ class _XylophoneAppState extends State<XylophoneApp> {
                     player.play('sound/note7.wav');
                   },
                   child: Container(
-                    child: Center(child: Text("Sİ",style:TextStyle(fontSize: 20,fontFamily: "Niveau"),)),
+                    child: Center(
+                        child: Text(
+                      "Sİ     B",
+                      style: TextStyle(fontSize: 20, fontFamily: "Niveau"),
+                    )),
                     width: double.infinity,
-                    height: 13.h,
+                    height: 10.h,
                     color: color7,
                   ),
                 ),
+
               ],
             ),
             Padding(
@@ -121,7 +266,11 @@ class _XylophoneAppState extends State<XylophoneApp> {
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       FloatingActionButton(
-                        child: Icon(Icons.color_lens_outlined,size: 25,),
+                        heroTag: "btn1",
+                        child: Icon(
+                          Icons.color_lens_outlined,
+                          size: 25,
+                        ),
                         backgroundColor: Colors.red,
                         onPressed: () {
                           setState(() {
@@ -141,7 +290,11 @@ class _XylophoneAppState extends State<XylophoneApp> {
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       FloatingActionButton(
-                        child: Icon(Icons.color_lens_outlined,size: 25,),
+                        heroTag: "btn2",
+                        child: Icon(
+                          Icons.color_lens_outlined,
+                          size: 25,
+                        ),
                         backgroundColor: Colors.green,
                         onPressed: () {
                           setState(() {
@@ -161,17 +314,21 @@ class _XylophoneAppState extends State<XylophoneApp> {
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       FloatingActionButton(
-                        child: Icon(Icons.color_lens_outlined,size: 25,),
+                        heroTag: "btn3",
+                        child: Icon(
+                          Icons.color_lens_outlined,
+                          size: 25,
+                        ),
                         backgroundColor: Colors.blue,
                         onPressed: () {
                           setState(() {
-                             color1 = Color(0xFF1976D2);
-                             color2 = Color(0xFF1E88E5);
-                             color3 = Color(0xFF2196F3);
-                             color4 = Color(0xFF42A5F5);
-                             color5 = Color(0xFF64B5F6);
-                             color6=  Color(0xFF90CAF9);
-                             color7 = Color(0xFFBBDEFB);
+                            color1 = Color(0xFF1976D2);
+                            color2 = Color(0xFF1E88E5);
+                            color3 = Color(0xFF2196F3);
+                            color4 = Color(0xFF42A5F5);
+                            color5 = Color(0xFF64B5F6);
+                            color6 = Color(0xFF90CAF9);
+                            color7 = Color(0xFFBBDEFB);
                           });
                         },
                       ),
@@ -181,7 +338,11 @@ class _XylophoneAppState extends State<XylophoneApp> {
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       FloatingActionButton(
-                        child: Icon(Icons.color_lens_outlined,size: 25,),
+                        heroTag: "btn4",
+                        child: Icon(
+                          Icons.color_lens_outlined,
+                          size: 25,
+                        ),
                         backgroundColor: Colors.orange,
                         onPressed: () {
                           setState(() {
@@ -196,7 +357,8 @@ class _XylophoneAppState extends State<XylophoneApp> {
                         },
                       ),
                     ],
-                  ),
+                  ),checkForAd(),
+
                 ],
               ),
             ),
